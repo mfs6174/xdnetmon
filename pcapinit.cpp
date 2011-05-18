9d8c5c78f38a5bce0ea9670bba081e486df9b9ee
Xijin Zhang <mfs6174@gmail.com>
2011-05-19 00:48:23 +0800

    加入了包结构,创建了需要的文件

--- pcapinit.cpp
+++ pcapinit.cpp
@@ -1,6 +1,8 @@
 #include "include.h"
 #include "h.h"
+#define ETHER_ADDR_LEN 6
 const int changdu=60;
+const int Ethchangdu=14;
 char ebuf[PCAP_ERRBUF_SIZE];
 char filter_exp[] = "(not (net 202.117.112.0 mask 255.255.240.0)) and (not (net 222.25.128.0 mask 255.255.192.0)) and (not (net 219.245.64.0 mask 255.255.192.0)) and (not (net 219.244.112.0 mask 255.255.240.0)) and (not ( (dst net 192.168.0.0 mask 255.255.252.0) and (src net 192.168.0.0 mask 255.255.252.0) ) ) and (not (net 210.27.0.0 mask 255.255.240.0))"; /* 过 滤表达式 */
 extern Shezhi shezhi;
@@ -12,6 +14,38 @@ const u_char *packet; /* 数据包 */
 string dev(shezhi.zhiding?(shezhi.dev):(pcap_lookupdev(ebuf)));
 pcap_t *pp;
 
+
+//开始包转换部分
+
+/* 以太网头 */
+struct sniff_ethernet {
+	u_char ether_dhost[ETHER_ADDR_LEN]; /* 目的地址 */
+	u_char ether_shost[ETHER_ADDR_LEN]; /* 源地址 */
+	u_short ether_type; /* IP? ARP? RARP? 等 */
+};
+
+/* IP 头 */
+struct sniff_ip {
+	u_char ip_vhl; /* version << 4 | header length >> 2 */
+	u_char ip_tos; /* type of service */
+	u_short ip_len; /* total length */
+	u_short ip_id; /* identification */
+	u_short ip_off; /* fragment offset field */
+#define IP_RF 0x8000 /* reserved fragment flag */
+#define IP_DF 0x4000 /* dont fragment flag */
+#define IP_MF 0x2000 /* more fragments flag */
+#define IP_OFFMASK 0x1fff /* mask for fragmenting bits */
+	u_char ip_ttl; /* time to live */
+	u_char ip_p; /* protocol */
+	u_short ip_sum; /* checksum */
+	struct in_addr ip_src,ip_dst; /* source and dest address */
+};
+#define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip) (((ip)->ip_vhl) >> 4)
+
+sniff_ip dangip;
+sniff_ethernet dangeth;
+
 void pcapinit()
 {
   pp=pcap_open_live(dev.c_str(),changdu,0,0,ebuf);
@@ -22,16 +56,13 @@ void pcapinit()
 
 void huidiao(u_char *args, const struct pcap_pkthdr *tou,const u_char *bao)
 {
-  gongzuo+=tou->len;
-  jiange+=tou->len;
-  cout<<tou->len<<endl;
-  /*
-  if (jiange>=_jiange)
-  {
-    shishi(gongzuo);
-    jiange=0;
-  }
-  */
+  // gongzuo+=tou->len;
+  // jiange+=tou->len;
+  // cout<<tou->len<<endl;
+  dangeth=(struct sniff_ethernet *)(packet);
+  dangip=(struct sniff_ip *)(packet+Ethchangdu);
+  id=hash();
+  pushmap(id,tou->len);
 }
 
 
