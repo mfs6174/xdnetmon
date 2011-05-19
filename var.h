55274ec0a215128dc0cf6d89a61dbc0fd3006ef5
Xijin Zhang <mfs6174@gmail.com>
2011-05-19 16:15:50 +0800

    修改了var.h，主要是设置变量的内容

--- var.h
+++ var.h
@@ -1,11 +1,12 @@
+#include "include.h"
 
 struct Shezhi
 {
   string dev;
-  bool baocun,zhiding;
-  long long keyong ;
+  bool mode,zhiding;
+  int  jiange;
 };
-long long gongzuo=0,jiange=0,benyue=0;
+long long zonghe=0;
 Shezhi shezhi;
 
-const int _mb_=1024*1024,_kb_=1024,_gb_=_mb_*1024,_jiange=512*_kb_;
+const int _mb_=1024*1024,_kb_=1024,_gb_=_mb_*1024;
