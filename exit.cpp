a15d346f5ba517ec7f0239bdc8389ecc01f0f383
mfs6174 <mfs6174@debian.acm>
2011-05-19 18:18:35 +0800

    写了一部分exit.cpp‘

--- exit.cpp
+++ exit.cpp
@@ -1,12 +1,12 @@
+#include "include.h"
+#include "h.h"
+extern pcap_t *pp;
 void tuichu()
 {
+  alarm(0);
   pcap_close(pp);
-  guanbixie();
-  cout<<"本次运行期间共产生流量"<<fixed<<setprecision(3)<<_mb(gongzuo)<<" 本月流量已用"<<_mb(gongzuo+benyue)<<" 剩余"<<_mb(shezhi.keyong-gongzuo-benyue)<<endl;
-  if (shezhi.baocun)
-  {
-    xiegongzuo(gongzuo);
-  }
+  timer();
+  cout<<"本次运行期间共产生流量"<<fixed<<setprecision(3)<<_mb(zonghe)<<" 本月流量已用"<<_mb(gongzuo+benyue)<<" 剩余"<<_mb(shezhi.keyong-gongzuo-benyue)<<endl;
   exit(0);
 }
 
