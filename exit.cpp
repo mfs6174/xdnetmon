e50b5018393ac9b6fd22aae181481490591d8aa5
Xijin Zhang <mfs6174@gmail.com>
2011-05-27 00:24:51 +0800

    写了一下todolist

--- exit.cpp
+++ exit.cpp
@@ -4,8 +4,8 @@ extern pcap_t *pp;
 extern Shezhi shezhi;
 void tuichu()
 {
-  alarm(0);
   pcap_close(pp);
+  alarm(0);
   if (!timer())
   {
     cout<<"本次运行期间共产生流量"<<fixed<<setprecision(3)<<_mb(zonghe)<<" 本月流量已用"<<_mb(getmon())<<" 剩余"<<_mb(shezhi.keyong-getmon())<<endl;
