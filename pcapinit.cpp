5d8066a34bce091732a52851dbcbe40c880e4a85
Xijin Zhang <mfs6174@gmail.com>
2011-06-11 02:52:33 +0800

    根据debug结果,修改了设置的网卡字符串的问题,原因还是未知(见注释处),现在能正常抓包,修改过滤表达式为合适形式

--- pcapinit.cpp
+++ pcapinit.cpp
@@ -4,14 +4,14 @@
 const int changdu=60;
 const int Ethchangdu=14;
 char ebuf[PCAP_ERRBUF_SIZE];
-char filter_exp[] = "(not host 127.0.0.1) and (ip net 192.168.0.0) and (not (net 202.117.112.0 mask 255.255.240.0)) and (not (net 222.25.128.0 mask 255.255.192.0)) and (not (net 115.155.0.0 mask 255.255.192.0)) and (not (net 219.245.64.0 mask 255.255.192.0)) and (not (net 219.244.112.0 mask 255.255.240.0)) and (not ( (dst net 192.168.0.0 mask 255.255.252.0) and (src net 192.168.0.0 mask 255.255.252.0) ) ) and (not (net 210.27.0.0 mask 255.255.240.0))"; /* 过 滤表达式 */
+char filter_exp[] = " (ip net 192.168.0.0 mask 255.255.252.0) and (not (net 202.117.112.0 mask 255.255.240.0)) and (not (net 222.25.128.0 mask 255.255.192.0)) and (not (net 115.155.0.0 mask 255.255.192.0)) and (not (net 219.245.64.0 mask 255.255.192.0)) and (not (net 219.244.112.0 mask 255.255.240.0)) and (not (net 210.27.0.0 mask 255.255.240.0)) and (not ( (dst net 192.168.0.0 mask 255.255.252.0) and (src net 192.168.0.0 mask 255.255.252.0) ) ) "; /* 过 滤表达式 */
 extern struct Shezhi shezhi;
 struct bpf_program fp;
 bpf_u_int32 mask; /* 网 络掩码 */
 bpf_u_int32 net; /* IP */
 struct pcap_pkthdr header; /* pcap头 */
 const u_char *packet; /* 数据包 */
-string dev(shezhi.zhiding?(shezhi.dev):(pcap_lookupdev(ebuf)));
+//string dev(shezhi.zhiding?(shezhi.dev):(pcap_lookupdev(ebuf))); //这句有问题,导致设备不对?
 pcap_t *pp;
 
 
@@ -48,8 +48,8 @@ sniff_ethernet *dangeth;
 
 void pcapinit() //初始化函数
 {
-  pp=pcap_open_live(dev.c_str(),changdu,0,0,ebuf);
-  pcap_lookupnet(dev.c_str(),&net,&mask,ebuf);
+  pp=pcap_open_live(shezhi.dev.c_str(),changdu,0,0,ebuf);
+  pcap_lookupnet(shezhi.dev.c_str(),&net,&mask,ebuf);
   pcap_compile(pp, &fp, filter_exp, 1, net);
   pcap_setfilter(pp, &fp);
 }
