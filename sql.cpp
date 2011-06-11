7729ce4a1107fe4739cc72c4db595514c6779ca2
Xijin Zhang <mfs6174@gmail.com>
2011-06-12 03:17:34 +0800

    每个文件都写了版权信息和GPL声明

--- sql.cpp
+++ sql.cpp
@@ -1,3 +1,26 @@
+/*
+ * File: sql.cpp 
+ * Author: Zhang Xijin(mfs6174)
+ * Email: mfs6174@gmail.com
+ *
+ * Copyright (C) 2011 Zhang Xijin(mfs6174)
+ *
+ * This file is part of XDnetmon.
+ *
+ * XDnetmon is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * XDnetmon is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with XDnetmon.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "include.h"
 #include "h.h"
 
