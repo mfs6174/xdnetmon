2ffda15c4b69067cda9c1c9d0dc6b7c1e0ce68ec
Xijin Zhang <mfs6174@gmail.com>
2011-06-26 23:38:10 +0800

    sql.cpp注释添加完成

--- sql.cpp
+++ sql.cpp
@@ -24,11 +24,11 @@
 #include "include.h"
 #include "h.h"
 
-sqlite3 *db=NULL;
-char *sqlerr=NULL;
-int sqlf;
-string yuju;
-extern Shezhi shezhi;
+sqlite3 *db=NULL;//数据库句柄
+char *sqlerr=NULL;//指向错误信息的字符指针
+int sqlf;//sql执行结果
+string yuju;//存放执行语句
+extern Shezhi shezhi;//var.cpp中的全局变量,设置
 
 void sqlgeterr(int s) //sql错误处理,判断是否出错,参数s为sql执行状态,输出错误信息,每次执行完sql语句调用一次
 {
@@ -45,9 +45,11 @@ void sqlinit() //sql数据库初始化,打开或新建数据库,如果新建,新
   sqlgeterr(sqlf);
   //以下新建2个表
   yuju="CREATE TABLE IF NOT EXISTS flow ( mac TEXT,ip TEXT,data INT,start DATETIME,end DATETIME);";
+  //表名:flow 表结构:mac ip为字符串,data为整数,单位是字节,start和end为起止时间,用unix时间戳表示
   sqlf=sqlite3_exec(db,yuju.c_str(),NULL,NULL,&sqlerr);
   sqlgeterr(sqlf);
   yuju="CREATE TABLE IF NOT EXISTS speed ( mac TEXT,ip TEXT,rate DOUBLE,end DATETIME);";
+  //表名:speed 表结构: mac ip为字符串,rate为浮点数,单位是字节/秒,end为本速度区间的结束时刻,unix时间戳表示
   sqlf=sqlite3_exec(db,yuju.c_str(),NULL,NULL,&sqlerr);
   sqlgeterr(sqlf);
 }
@@ -71,17 +73,18 @@ string str(double x)//浮点数转为字符串,构造sql语句用
   return t.str();
 }
 
-string getmac(const string &ss)//在id串中提取mac地址部分
+string getmac(const string &ss)//在id串中提取mac地址部分 ss是构造好的ID串,返回mac字符串
 {
   return ss.substr(0,ss.find("##"));
 }
 
-string getip(const string &ss) //在id串中提取ip地址部分
+string getip(const string &ss) //在id串中提取ip地址部分 ss是构造好的ID串,返回ip字符串
 {
  return ss.substr(ss.find("##")+2);
 }
 
 void sqlflow(const string &ss,long long liu,long long kai,long long tt) //向数据库写入流量信息
+//ss是ID字符串,liu是流量,kai是起始时间,tt是终止时间
 {
   char **jieguo=NULL;
   int hang=0,lie=0;
@@ -106,14 +109,17 @@ void sqlflow(const string &ss,long long liu,long long kai,long long tt) //向数
     }
     yuju="UPDATE flow SET data=data+"+str(liu)+",end='"+str(tt)+"' WHERE mac='"+mac+"' AND ip='"+ip+"' AND (";
     yuju+=str(tt)+"-start<"+str(shezhi.pian)+");";
+    //更新语句,更新间隔小于时间片的记录,最后,同一个IP MAC组合的每条记录间隔都大于时间片,期间的流量累加
     sqlf=sqlite3_exec(db,yuju.c_str(),NULL,NULL,&sqlerr);
     sqlgeterr(sqlf);
   } 
 }
 
 void sqlspeed(const string &ss,long long liu,long long tt) //向数据库写入速度信息
+//ss是ID串,liu是流量,tt是截止时间
 {
   yuju="INSERT INTO speed VALUES ('"+getmac(ss)+"','"+getip(ss)+"',"+str(liu/shezhi.jiange)+",'"+str(tt)+"')";
+  //插入语句,每次插入此IP MAC组合数据库更新间隔中的平均速度
   sqlf=sqlite3_exec(db,yuju.c_str(),NULL,NULL,&sqlerr);
   sqlgeterr(sqlf);
 }
