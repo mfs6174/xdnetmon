056ba8fd342a2b8bac01f4306ba4d5f5ceb0c18b
Xijin Zhang <mfs6174@gmail.com>
2011-06-28 23:57:02 +0800

    给pacpinit.cpp加注释

--- pcapinit.cpp
+++ pcapinit.cpp
@@ -24,9 +24,9 @@
 #include "include.h"
 #include "h.h"
 #define ETHER_ADDR_LEN 6
-const int changdu=60;
-const int Ethchangdu=14;
-char ebuf[PCAP_ERRBUF_SIZE];
+const int changdu=60;//抓包最大的长度,期望减少复制工作,增加效率
+const int Ethchangdu=14; //以太头长度
+char ebuf[PCAP_ERRBUF_SIZE];//错误信息缓冲
 char filter_exp[] = " (ip net 192.168.0.0 mask 255.255.252.0) and (not (net 202.117.112.0 mask 255.255.240.0)) and (not (net 222.25.128.0 mask 255.255.192.0)) and (not (net 115.155.0.0 mask 255.255.192.0)) and (not (net 219.245.64.0 mask 255.255.192.0)) and (not (net 219.244.112.0 mask 255.255.240.0)) and (not (net 210.27.0.0 mask 255.255.240.0)) and (not ( (dst net 192.168.0.0 mask 255.255.252.0) and (src net 192.168.0.0 mask 255.255.252.0) ) ) "; /* 过 滤表达式 */
 extern struct Shezhi shezhi;
 struct bpf_program fp;
@@ -35,7 +35,7 @@ bpf_u_int32 net; /* IP */
 struct pcap_pkthdr header; /* pcap头 */
 const u_char *packet; /* 数据包 */
 //string dev(shezhi.zhiding?(shezhi.dev):(pcap_lookupdev(ebuf))); //这句有问题,导致设备不对?
-pcap_t *pp;
+pcap_t *pp; //pcap句柄
 
 
 //开始包转换部分
@@ -66,10 +66,10 @@ struct sniff_ip {
 #define IP_HL(ip) (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip) (((ip)->ip_vhl) >> 4)
 
-sniff_ip *dangip;
-sniff_ethernet *dangeth;
+sniff_ip *dangip; //IP头指针
+sniff_ethernet *dangeth; //以太头指针
 
-void pcapinit() //初始化函数
+void pcapinit() //初始化函数,由main()在程序启动时调用,开启pcap句柄,编译过滤表达式,设置过滤器
 {
   pp=pcap_open_live(shezhi.dev.c_str(),changdu,0,0,ebuf);
   pcap_lookupnet(shezhi.dev.c_str(),&net,&mask,ebuf);
@@ -77,27 +77,28 @@ void pcapinit() //初始化函数
   pcap_setfilter(pp, &fp);
 }
 
-void uc2mac(string &rr,u_char ss[])
+void uc2mac(string &rr,u_char ss[])//将usighed char类型的mac地址转换为6段16进制可读字符串,rr存储结果字符串,ss是待转换的字符数组
 {
   static char macfu[18]="0123456789ABCDEF";
   rr="0";
   int i;
   for (i=0;i<ETHER_ADDR_LEN;i++)
   {
+    //分别取出unsigned char对应16进制的两位,存入字符串
     rr.push_back(':');
     rr.push_back(macfu[ss[i]>>4]);
     rr.push_back(macfu[ss[i]%16]);
   }
 }
     
-void zuoid(string &s,bool fl) //转换mac和ip为一个id字符串
+void zuoid(string &s,bool fl) //转换mac和ip为一个id字符串,s是存放结果的字符串,fl是标志,为true时源方向是内网计算机
 {
   string ss;
   if (fl)
   {
-    uc2mac(s,dangeth->ether_shost);
-    ss=inet_ntoa(dangip->ip_src);
-    s=s+"##"+ss;
+    uc2mac(s,dangeth->ether_shost);//取出以太头中的源mac,转换为可读形式放入s
+    ss=inet_ntoa(dangip->ip_src);//取出IP头中的源IP,转换为点分10进制的字符数组,在赋值给字符串ss
+    s=s+"##"+ss;//用##标志分隔mac与IP
   }
   else
   {
@@ -108,13 +109,14 @@ void zuoid(string &s,bool fl) //转换mac和ip为一个id字符串
 }
 
 void huidiao(u_char *args, const struct pcap_pkthdr *tou,const u_char *bao)//回调函数，解析数据包，写入map
+  //pcap loop的回调函数,第一个为参数,未使用,tou是抓到数据包的头指针,bao是实际包数据块的指针
 {
-  string id="";
+  string id="";//初始化存储ID的字符串类
   bool fl=false;
-  static bpf_u_int32 pan=net&mask;
-  dangeth=(struct sniff_ethernet *)(bao);
-  dangip=(struct sniff_ip *)(bao+sizeof(sniff_ethernet));
-  if (((dangip->ip_src.s_addr)&mask)==pan)//卧槽,这个==前不加括号就他喵的不对,这是神马神运算顺序!
+  static bpf_u_int32 pan=net&mask;//静态变量,判定标志,即内网的掩码和IP段的按位与
+  dangeth=(struct sniff_ethernet *)(bao);//强制转换数据块指针为以太头指针,即取最前的一块数据对应以太头的结构
+  dangip=(struct sniff_ip *)(bao+sizeof(sniff_ethernet));//强制转换数据块指针加上以太头的偏移量为IP头
+  if (((dangip->ip_src.s_addr)&mask)==pan)//卧槽,这个==前不加括号就他喵的不对,这是神马神运算顺序!//于是判断哪个方向是内网,设置传送给zuoid()的fl
   {
     fl=true;
   }
@@ -122,8 +124,8 @@ void huidiao(u_char *args, const struct pcap_pkthdr *tou,const u_char *bao)//回
   {
     fl=false;
   }
-  zuoid(id,fl);
-  pushmap(id,tou->len);
+  zuoid(id,fl);//生成mac和IP组合对应的ID字符串
+  pushmap(id,tou->len);//调用map.cpp中的pushmap(),将这个包的大小存入缓存map
 }
 
 
