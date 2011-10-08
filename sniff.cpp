34361b257bf064133402f2d263f993b185a74aaf
Xijin Zhang <mfs6174@gmail.com>
2011-10-08 18:37:02 +0800

    不同版本的定义不同，先自己复制了一个

--- sniff.cpp
+++ sniff.cpp
@@ -24,10 +24,73 @@
 #include "include.h"
 #include "h.h"
 
+struct stcphdr
+  {
+    u_int16_t th_sport;		/* source port */
+    u_int16_t th_dport;		/* destination port */
+    tcp_seq th_seq;		/* sequence number */
+    tcp_seq th_ack;		/* acknowledgement number */
+#  if __BYTE_ORDER == __LITTLE_ENDIAN
+    u_int8_t th_x2:4;		/* (unused) */
+    u_int8_t th_off:4;		/* data offset */
+#  endif
+#  if __BYTE_ORDER == __BIG_ENDIAN
+    u_int8_t th_off:4;		/* data offset */
+    u_int8_t th_x2:4;		/* (unused) */
+#  endif
+    u_int8_t th_flags;
+#  define TH_FIN	0x01
+#  define TH_SYN	0x02
+#  define TH_RST	0x04
+#  define TH_PUSH	0x08
+#  define TH_ACK	0x10
+#  define TH_URG	0x20
+    u_int16_t th_win;		/* window */
+    u_int16_t th_sum;		/* checksum */
+    u_int16_t th_urp;		/* urgent pointer */
+};
+
+# else /* !__FAVOR_BSD */
+struct tcphdr
+  {
+    u_int16_t source;
+    u_int16_t dest;
+    u_int32_t seq;
+    u_int32_t ack_seq;
+#  if __BYTE_ORDER == __LITTLE_ENDIAN
+    u_int16_t res1:4;
+    u_int16_t doff:4;
+    u_int16_t fin:1;
+    u_int16_t syn:1;
+    u_int16_t rst:1;
+    u_int16_t psh:1;
+    u_int16_t ack:1;
+    u_int16_t urg:1;
+    u_int16_t res2:2;
+#  elif __BYTE_ORDER == __BIG_ENDIAN
+    u_int16_t doff:4;
+    u_int16_t res1:4;
+    u_int16_t res2:2;
+    u_int16_t urg:1;
+    u_int16_t ack:1;
+    u_int16_t psh:1;
+    u_int16_t rst:1;
+    u_int16_t syn:1;
+    u_int16_t fin:1;
+#  else
+#   error "Adjust your <bits/endian.h> defines"
+#  endif
+    u_int16_t window;
+    u_int16_t check;
+    u_int16_t urg_ptr;
+};
+
+#define TH_OFF(th) (((th)->th_off & 0xf0) >> 4)
+
 #define NODE 100000 //最大可能出现的节点数
 #define CH 63 //分支数
 
-tcphdr *dangtcp;
+stcphdr *dangtcp;
 u_char *fuzai;
 bool yichuli;
 
@@ -116,7 +179,7 @@ void acinit()//自动机初始化，执行完以后zh里就是goto或fail的位�
   }
 }
 
-bool com(char *s) //查找每个模式串出现的次数
+bool com(u_char *s) //查找每个模式串出现的次数
 {
   int p=0;
   for (;*s;s++)
@@ -134,8 +197,6 @@ bool com(char *s) //查找每个模式串出现的次数
   }
   return false;
 }
-
-#define TH_OFF(th) (((th)->th_off & 0xf0) >> 4)
 
 string str(long long x)//长整形转为字符串,用于构造sql语句串
 {
